8bd44412932b68c2e4d2c6fb6bc1bba6f1053781
Advait Sachin Dhamdhere <adhamdhe@silo.soic.indiana.edu>
2020-08-31 16:49:18 -0400

    Adding Assignment1 changes to Hello.c file

--- exercises/wk1/hello.c
+++ exercises/wk1/hello.c
@@ -1,15 +1,21 @@
 /* 
- * Basic C program, Assignment 0, 8/24 
+ * Basic C program, Assignment 1, 9/2 
  * Author: Advait Dhamdhere
 */
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
-  
-  printf("Advait Dhamdhere.\n");
-  printf("I have been alive for 7418 days.");
-  return(0); 
+ 	 printf("\"\" {Welcome to Fall2020 - 'Corona-affected' 8-Week session @ IU}, \\\\ Every week, please start early and ask for help! Also, let's try to stay safe! // \"\" \n");
+ 	 int num = 100; // starting number
+	 while(1){
+		printf("%d %s\n",num, ".....");
+		sleep(1);
+		num--;
+
+  	 }
+ 	 return(0); 
 }
 
 
